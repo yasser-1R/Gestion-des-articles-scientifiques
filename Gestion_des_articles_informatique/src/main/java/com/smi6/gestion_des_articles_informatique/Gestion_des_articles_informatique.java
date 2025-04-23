c00dbbff235080d52930780a36ff3126019e747a
yasser-1R <rochdiyasser0@gmail.com>
2025-04-23 13:35:33 +0100

    initialisation_netbeans

---
+++ Gestion_des_articles_informatique/src/main/java/com/smi6/gestion_des_articles_informatique/Gestion_des_articles_informatique.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.smi6.gestion_des_articles_informatique;
+
+/**
+ *
+ * @author YN
+ */
+public class Gestion_des_articles_informatique {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
